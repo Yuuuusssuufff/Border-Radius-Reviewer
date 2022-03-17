64ab87ec86b691f36b066ef1edd4f27e747d0588
AjibolaYusuf <ajibolayusuf8@gmail.com>
2022-03-17 17:35:37 +0100

    project completion

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Getting all out elements
+// Getting all out elements/variables
 const innerEl = document.querySelector(".inner");
 const input1El = document.querySelector("#value1");
 const input2El = document.querySelector("#value2");
@@ -10,7 +10,6 @@ const topLeftEl = document.querySelector("#prop1");
 const topRightEl = document.querySelector("#prop2");
 const bottomRightEl = document.querySelector("#prop3");
 const bottomLeftEl = document.querySelector("#prop4");
-const body = document.body;
 const modalEl = document.querySelector("#modal");
 
 // Triggers (addEventListeners)
@@ -20,50 +19,38 @@ buttonEl.addEventListener("click", () => {
 
 buttonEl.addEventListener("click", () => {
   modalEl.style.display = "flex";
+  modalEl.style.background = "red";
+  modalEl.style.cursor = "pointer"
 });
 
-modalEl.addEventListener("mouseover", () => {
+modalEl.addEventListener("mouseout", () => {
   modalEl.style.display = "none";
 });
 //Top Left
 input1El.addEventListener("input", () => {
-  // topLeftEl.textContent = e.target.value
-
   let topLeft;
-  topLeft = `${parseInt(input1El.value)}px`;
-  console.log(topLeft);
+  topLeft = `${input1El.value}px`;
   innerEl.style.borderTopLeftRadius = topLeft;
   topLeftEl.textContent = topLeft;
 });
 
 input2El.addEventListener("input", () => {
-  // topRightEl.innerText = e.target.value
-
   let topRight;
-  topRight = `${parseInt(input2El.value)}px`;
+  topRight = `${input2El.value}px`;
   innerEl.style.borderTopRightRadius = topRight;
   topRightEl.textContent = topRight;
 });
 
 input3El.addEventListener("input", () => {
-  // bottomRightEl.innerText = e.target.value
   let bottomRight;
-  bottomRight = `${parseInt(input3El.value)}px`;
+  bottomRight = `${input3El.value}px`;
   innerEl.style.borderBottomRightRadius = bottomRight;
   bottomRightEl.textContent = bottomRight;
 });
 
 input4El.addEventListener("input", () => {
-  // bottomLeftEl.innerText = e.target.value
   let bottomLeft;
-  bottomLeft = `${parseInt(input4El.value)}px`;
-  console.log(bottomLeft);
+  bottomLeft = `${input4El.value}px`;
   innerEl.style.borderBottomLeftRadius = bottomLeft;
   bottomLeftEl.textContent = bottomLeft;
 });
-
-// displayEl.textContent = `border-radius: ${topLeftEl} ${topRightEl}px ${bottomRightEl}px ${bottomLeftEl}px;`
-
-// We need to create a modal
-
-// A modal is a dialog box/popup window that is displayed on top of the current page:
